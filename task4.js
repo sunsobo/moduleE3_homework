4537adf6eaec87791da9e86c724d0e0fc690ac83
sunsobo <138815704+sunsobo@users.noreply.github.com>
2024-04-01 13:44:16 +0300

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,1 +1,13 @@
+function printNumbers(start, end) {
+    let current = start;
+    const intervalId = setInterval(function() {
+        console.log(current);
+        if (current === end) {
+            clearInterval(intervalId);
+        }
+        current++;
+    }, 1000);
+}
 
+// Пример использования функции
+printNumbers(5, 15);
